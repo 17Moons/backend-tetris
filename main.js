af515c3c03d7f2eca56560b86297e8009763d4e1
Vaibhav Singh <92429890+17Moons@users.noreply.github.com>
2022-07-11 09:27:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,164 @@
+const http = require ('http');
+const path = require('path');
+const fs = require('fs');
+const WebSocketServer = require('ws').Server;
+const Session = require('./session');
+const Client = require('./client');
+
+const http_server = http.createServer((req, res) => {     
+    // Build file path
+    let filePath = path.join(
+      __dirname,
+      '../',      
+      req.url === '/' ? './index.html' : req.url
+    );
+  
+    // Extension of file
+    let extname = path.extname(filePath);
+  
+    // Initial content type
+    let contentType = 'text/html';
+  
+    // Check ext and set content type
+    switch (extname) {
+      case '.js':
+        contentType = 'text/javascript';
+        break;
+      case '.css':
+        contentType = 'text/css';
+        break;
+      case '.json':
+        contentType = 'application/json';
+        break;
+      case '.png':
+        contentType = 'image/png';
+        break;
+      case '.jpg':
+        contentType = 'image/jpg';
+        break;
+    }
+  
+    // Read File    
+    fs.readFile(filePath, (err, content) => {
+      if (err) {
+        if (err.code == 'ENOENT') {
+          // Page not found
+          fs.readFile(
+            path.join(__dirname, '404.html'),
+            (err, content) => {
+              res.writeHead(200, { 'Content-Type': 'text/html' });
+              res.end(content, 'utf8');
+            }
+          );
+        } else {
+          //  Some server error
+          res.writeHead(500);
+          res.end(`Server Error: ${err.code}`);
+        }
+      } else {
+        // Success
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(content, 'utf8');
+      }
+    });
+  });
+  
+const PORT = process.env.PORT || 9000;
+  
+http_server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+const server = new WebSocketServer({server: http_server});
+
+const sessions = new Map;
+
+const createId = (len = 6, chars = 'abcdefghjkmnopqrstwxyz0123456789') => {
+    let id = '';
+    while (len--) {
+        id += chars[Math.random() * chars.length | 0];
+    }
+    return id;
+
+}
+
+const createClient = (conn, id = createId()) => {
+    return new Client(conn, id);
+}
+
+const createSession = (id = createId()) => {
+    if (sessions.has(id)) {
+        throw new Error(`Session ${id} already exists`);
+    }
+    const session = new Session(id);
+    console.log('Creating session', session);
+
+    sessions.set(id, session);
+
+    return session;
+}
+
+const getSession = id => sessions.get(id);
+
+const broadcastSession = (session) => {
+    const clients = [...session.clients];
+    clients.forEach(client => {
+        client.send({
+            type: 'session-broadcast',
+            peers: {
+                you: client.id,
+                clients: clients.map(client => {
+                    return {
+                        id: client.id,
+                        state: client.state,
+                    }
+                })
+            },
+
+        });
+    })
+}
+
+server.on('connection', conn => {
+    console.log('Connection established');
+    const client = createClient(conn);
+
+    conn.on('message', msg => {
+        console.log('Message received', msg);
+        const data = JSON.parse(msg);
+
+        if (data.type === 'create-session') {            
+            const session = createSession();
+            session.join(client);
+            client.state = data.state;            
+            client.send({
+                type: 'session-created',
+                id: session.id,
+        });
+    } else if (data.type === 'join-session') {
+        const session = getSession(data.id) || createSession(data.id);
+        session.join(client);
+
+        client.state = data.state;
+        broadcastSession(session);
+    } else if (data.type === 'state-update') {
+        const [prop, value] = data.state;
+        client.state[data.fragment][prop] = value;
+        client.broadcast(data);
+    }      
+    });
+
+    conn.on('close', () => {
+        console.log('Connection closed');
+        const session = client.session;
+        if (session) {
+            session.leave(client);
+            if (session.clients.size === 0) {
+                sessions.delete(session.id);
+            }
+        }
+
+        broadcastSession(session);
+    });
+
+});
